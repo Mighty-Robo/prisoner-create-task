c970bb29b7545c4321595a2a889217e06dac85c4
Ari Vasquez <ariangel1004@gmail.com>
2023-09-14 19:01:00 +0000

    Added way to add new prisoners

--- period4.py
+++ period4.py
@@ -1,5 +1,9 @@
 print("Welcome to the Silly Billy County prisoner sorting system")
 prisonerList = ["Jack Nelson", "Brooke Silly", "Lucas Cameron"]
-nav = input("Would you like to view the current prisoner list (A) or register a new prisoner? (B)").lower()
-if nav == "A" :
+nav = input("Would you like to view the current prisoner list (1) or register a new prisoner? (2)")
+if nav == "1":
+    print(prisonerList)
+elif nav == "2":
+    new_prisoner = input("What is the prisoners name?")
+    prisonerList.insert(0,new_prisoner)
     print(prisonerList)
