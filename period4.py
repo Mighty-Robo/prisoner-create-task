f38d89b93237399d13bdaec58c3b813f3b210b17
Ari Vasquez <ariangel1004@gmail.com>
2024-03-12 18:58:49 +0000

    fixed by emmett

--- period4.py
+++ period4.py
@@ -1,9 +1,32 @@
-print("Welcome to the Silly Billy County prisoner sorting system")
-prisonerList = ["Jack Nelson", "Brooke Silly", "Lucas Cameron"]
-nav = input("Would you like to view the current prisoner list (1) or register a new prisoner? (2)")
-if nav == "1":
-    print(prisonerList)
-elif nav == "2":
-    new_prisoner = input("What is the prisoners name?")
-    prisonerList.insert(0,new_prisoner)
-    print(prisonerList)
+print("Welcome to the Silly Billy County prisoner records system")
+prisonerList = ["Jack Nelson", "Brooke Silly", "Lucas Bucas"]
+
+logout = False
+while logout == False:
+    def nav():
+        choice = input("Would you like to view the current prisoner list (1) or register a new prisoner? (2) ")
+        return choice
+
+    option = nav()
+
+    def addPrisoner (prisoner, pList):
+    
+        pList.insert(0,new_prisoner)
+        return pList
+    
+    if option == "1":
+        print(prisonerList)
+        logoutQ = input("Log out? Y/N: ")
+        if logoutQ == "Y":
+            logout = True
+    
+    elif option == "2":
+        new_prisoner = input("What is the prisoners name? ")
+        addPrisoner(new_prisoner, prisonerList)
+        print("New prisoner has been added to list: " + new_prisoner)
+        print(prisonerList)
+        logoutQ = input("Log out? Y/N: ")
+        if logoutQ == "Y":
+            logout = True
+
+    
