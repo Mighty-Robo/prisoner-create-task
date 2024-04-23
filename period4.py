286412a2eabb37793a45f27f7744475d80605469
emmett reibel <emmett.reibel@gmail.com>
2024-04-23 18:28:29 +0000

    meets requirements now i think

--- period4.py
+++ period4.py
@@ -2,31 +2,29 @@ print("Welcome to the Silly Billy County prisoner records system")
 prisonerList = ["Jack Nelson", "Brooke Silly", "Lucas Bucas"]
 
 logout = False
-while logout == False:
-    def nav():
-        choice = input("Would you like to view the current prisoner list (1) or register a new prisoner? (2) ")
-        return choice
 
-    option = nav()
+def nav():
+    choice = input("Would you like to view the current prisoner list (1) or register a new prisoner? (2) ")
+    return choice
+# ^ function that lets you choose options
 
-    def addPrisoner (prisoner, pList):
+ 
+def Prison (logout, pList):
+    while logout == False:
+        option = nav()
+        if option == "1":
+            print(prisonerList) #shows prisoner list
+            logoutQ = input("Log out? Y/N: ") #allows user to stop program
+            if logoutQ == "Y":
+                logout = True
     
-        pList.insert(0,new_prisoner)
-        return pList
-    
-    if option == "1":
-        print(prisonerList)
-        logoutQ = input("Log out? Y/N: ")
-        if logoutQ == "Y":
-            logout = True
-    
-    elif option == "2":
-        new_prisoner = input("What is the prisoners name? ")
-        addPrisoner(new_prisoner, prisonerList)
-        print("New prisoner has been added to list: " + new_prisoner)
-        print(prisonerList)
-        logoutQ = input("Log out? Y/N: ")
-        if logoutQ == "Y":
-            logout = True
+        elif option == "2": #allows user to add prisoner
+            new_prisoner = input("What is the prisoners name? ") 
+            pList.insert(0,new_prisoner)
+            print("New prisoner has been added to list: " + new_prisoner)
+            print(prisonerList)
+            logoutQ = input("Log out? Y/N: ")
+            if logoutQ == "Y":
+                logout = True
 
-    
+Prison (logout, prisonerList) #feed logout into the function directly. it works i guess
